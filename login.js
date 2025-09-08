d07f9b1ef8823795234c59200175abd1f653be97
Ringgustav <gurra.rock@gmail.com>
2025-09-08 15:34:39 +0200

    element och console loggar

--- login.js
+++ login.js
@@ -1,3 +1,29 @@
 //variabler för rätt username/password
 const correctUsername = "text";
 const correctPassword = "1234";
+
+//väntar på hela html dokumentet innan js körs
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("sidan är laddad");
+
+    //loginknappen lyssnar på click och input vad användaren skrev
+    const loginBtn = document.getElementById("loginBtn");
+    const usernameInput = document.getElementById("usernameInput");
+    const passwordInput = document.getElementById("passwordInput");
+
+    //dem olika sidorna (menyn, vid rätt inlog och om det inte lyckades)
+    const loginView = document.getElementById("login");
+    const successView = document.getElementById("success");
+    const failView = document.getElementById("fail");
+
+    //hämtar försökigen knappen
+    const tryAgainBtn = document.getElementById("tryAgainBtn");
+
+    //se så att element hittades
+    console.log("element hittades:");
+    console.log(loginBtn);
+    console.log(usernameInput);
+    console.log(passwordInput);
+    console.log(successView);
+    console.log(failView);
+})
